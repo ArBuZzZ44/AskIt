d119cc1e468b71c84ff60695e2edb535a06a1698
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-31 14:06:11 +0700

    add route questions/new

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    root "pages#index"
    get '/questions', to: 'questions#index' # localgost/questions 
+   get '/questions/new', to: 'questions#new'
 end
