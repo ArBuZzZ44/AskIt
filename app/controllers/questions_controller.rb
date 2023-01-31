f1ec84dc13be5e0dfe7d2d46434f08e9b26c3314
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-31 14:20:37 +0700

    add create method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,19 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
