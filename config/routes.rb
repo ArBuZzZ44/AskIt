c61a9634b569871f9e723d0bbb237897452b0a1c
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-31 14:35:37 +0700

    add route questions#update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:index, :new, :edit, :create]
+  resources :questions, only: [:index, :new, :edit, :create, :update]
 
   root "pages#index"
 end
