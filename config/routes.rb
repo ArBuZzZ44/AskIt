91e0947d8a35ef25f1a8035fb0c876f0767f6caf
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 17:44:55 +0700

    add route for destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :questions do 
-    resources :answers, only: %i[create]
+    resources :answers, only: %i[create destroy]
   end
 
   root "pages#index"
