2d9eaf835ee5e4da3d9d61d021ec9b80e223660b
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 16:51:52 +0700

    migrate table answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_134708) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_095047) do
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_134708) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
