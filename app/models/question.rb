b061ac51d4a98fa357d8c1d7695884b39fd45566
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 17:45:49 +0700

    add dependence for destroy

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :answers
+  has_many :answers, dependent: :destroy # если удалить question, то все зависимые ответы тоже долнжы удалиться
   validates :title, presence: true, length: {minimum: 2}
   validates :body, presence: true, length: {minimum: 2}
 
