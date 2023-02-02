02ae391b97d0862ae8d990728d6eac1cfb00a281
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 17:31:00 +0700

    added a variable for all answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,6 +21,7 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
   end
 
   def edit 
