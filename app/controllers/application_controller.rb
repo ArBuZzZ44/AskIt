5401e5a69a13e3949f35c61ab5af55b05c36d854
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 16:16:25 +0700

    error message output for non-existent question/id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :notfound #значит обрабатывать ошибку ActiveRecord в методе notfound
+
+  private
+
+  def notfound(exception) # exception - ошибка, которая произошла
+    logger.warn exception # записывает в журнал событий rails данную ошибку
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -47,6 +47,6 @@ class QuestionsController < ApplicationController
   end
 
   def set_question
-    @question = Question.find_by id: params[:id]
+    @question = Question.find params[:id]
   end
 end
