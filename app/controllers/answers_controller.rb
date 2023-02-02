b355e7b9d604a6843650aacfebf215454b02025f
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-02 17:14:44 +0700

    add method create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,24 @@
 class AnswersController < ApplicationController
+  before_action :set_question!
+
+  def create
+    @answer = @question.answers.build answer_params
+
+    if @answer.save
+      flash[:success] = "Answer created!"
+      redirect_to question_path(@question)
+    else
+      render 'questions/show'
+    end
+  end
+
+  private
+
+  def set_question!
+    @question = Question.find params[:question_id]
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
