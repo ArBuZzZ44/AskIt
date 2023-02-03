f22b67f88bb9c95c481b4bbb4447828ba8c16d86
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-04 02:31:24 +0700

    add helper for title page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "AskIt"
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
