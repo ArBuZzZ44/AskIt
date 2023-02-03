a2279e1596204a58fde6570cb8b4623e849d1a00
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-04 03:16:24 +0700

    created a helper to indicate the current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 module ApplicationHelper
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+
+    css_class = current_page == title ? 'text-secondary' : 'text-white' # если заголовк равен текущей странице, то text-secondar
+
+    options[:class] = if options[:class]
+        options[:class] + ' ' + css_class
+      else
+        css_class
+      end
+
+    link_to title, url, options
+  end
+
+  def currently_at(current_page = "")
+    # значение current_page, которая передается методу, дожна передоваться локальной переменной current_page:
+    render partial: 'shared/menu' , locals: {current_page: current_page} # пишем именно partial, т.к. может вывести и partial и layout
+  end
+
   def full_title(page_title = "")
     base_title = "AskIt"
     if page_title.present?
