9bcb27f0ea81a0c3b6eadce5206fc3fb9d45c284
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-05 02:49:07 +0700

    create test questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+30.times do 
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+  title = Faker::Hipster.sentence(word_count: 3)
+  Question.create title: title, body: body
+end
