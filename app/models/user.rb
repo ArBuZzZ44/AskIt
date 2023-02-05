149f63b7fb6c08a0697f1879f8a79e608aadfc4c
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-05 23:55:34 +0700

    add name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password # создает виртуальный атрибут password и password_confirmation. Они не попадают в бд, в бд идет только хеш пароля в password_digest
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
