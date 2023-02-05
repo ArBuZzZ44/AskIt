885a3398bc0f04842630fb9189f94fd46506469f
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-06 00:13:27 +0700

    add session for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the AskIt, #{@user.name}"
       redirect_to root_path
     else
