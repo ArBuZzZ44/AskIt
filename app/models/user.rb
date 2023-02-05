a96f323c3071361c1c65585f67b29326622e6412
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-05 23:34:53 +0700

    add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password # создает виртуальный атрибут password и password_confirmation. Они не попадают в бд, в бд идет только хеш пароля в password_digest
+
+  validates :email, presence: true, uniqueness: true
 end
