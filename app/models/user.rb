d856971875464c1fc2a60501ef7da21abe29831e
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-06 13:29:14 +0700

    remove name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_secure_password # создает виртуальный атрибут password и password_confirmation. Они не попадают в бд, в бд идет только хеш пароля в password_digest
 
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
 end
