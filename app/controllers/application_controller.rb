24e488481ffc47340816cfeb2cd5541143e3db13
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-06 13:47:57 +0700

    we have taken out methods for displaying the user from the controller to the concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
-
-  private
+  include Authentication
   
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present? # задекорировали, теперь метод можем использовать
-  end
-
-  def user_signed_in?
-    current_user.present?
-  end
-
-  helper_method :current_user, :user_signed_in? # делаем хелперы из методов, чтобы они были доступны в представлениях
 end
---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,18 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  included do 
+
+  private
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present? # задекорировали, теперь метод можем использовать
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  helper_method :current_user, :user_signed_in? # делаем хелперы из методов, чтобы они были доступны в представлениях
+  end
+end
