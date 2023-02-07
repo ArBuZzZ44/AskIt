80687af0dfeaebaf884283aaf4ac4808044ffe84
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-07 20:47:36 +0700

    update routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sessions, only: %i[new create destroy] # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
+  resource :session, only: %i[new create destroy] # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
 
   resources :users, only: %i[new create]
 
