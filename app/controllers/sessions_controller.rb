df9d9ffbbc7fe8403ec2ded694e6ceb16bdad2df
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-07 20:24:59 +0700

    add data processing in create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render plain: params.to_yaml
+    user = User.find_by email: params[:email]
+    # добавляем амперсант, если user будет nil, то данное условие сразу обратиться в ложное и мы уйдем в ветку else
+    if user&.authenticate(params[:password]) # метод authenticate добавляется благодаря has_secure_password в моделт User. принимает строку, конвертирует ее в хеш и сравнивает с хешем в бд
+      sign_in user
+      flash[:success] = "Welcome back, #{current_user.name_or_email}!"
+      redirect_to root_path
+    else
+      flash[:warning] = "Incorrect email and/or password"
+      redirect_to new_session_path
+    end
   end
 
   def destroy
